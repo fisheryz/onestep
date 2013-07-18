7fd7f122b89240b570dcdad338d05c950ece3212
fisheryz <fisheryz@hotmail.com>
2013-07-18 11:58:06 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onestep::Application.config.secret_key_base = '4cee9215661bcec709e982cb28e79d8c5b9125e77261cda4bf2caca6d047fc8d5dc16fec43e691d6e30b1aeb850d673f53feffcabcacdea97404645b4e8c306e'
